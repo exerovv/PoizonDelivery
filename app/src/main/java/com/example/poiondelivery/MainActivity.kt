00a28ca47be0ab9267f65c54d47c1d42f8afc52f
emir <accurse1337@gmail.com>
2024-11-05 11:13:24 +0300

    Добавлен минимальный функционал

--- app/src/main/java/com/example/poiondelivery/MainActivity.kt
+++ app/src/main/java/com/example/poiondelivery/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.poiondelivery
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val userLogin : EditText = findViewById(R.id.email_log)
+        val userPassword : EditText = findViewById(R.id.pass_log)
+        val button : Button = findViewById(R.id.button_log)
+        val regLink : TextView = findViewById(R.id.no_acc)
+
+        regLink.setOnClickListener{
+            val intent = Intent(this, RegActivity::class.java)
+            startActivity(intent)
+        }
+
+        button.setOnClickListener{
+            val login = userLogin.text.toString().trim()
+            val password = userPassword.text.toString().trim()
+            if(login == "" || password == ""){
+                Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/poiondelivery/RegActivity.kt
+++ app/src/main/java/com/example/poiondelivery/RegActivity.kt
@@ -1,4 +1,35 @@
 package com.example.poiondelivery
 
-class RegActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
+
+class RegActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg)
+
+        val userLogin : EditText = findViewById(R.id.email_reg)
+        val userPassword : EditText = findViewById(R.id.pass_reg)
+        val button : Button = findViewById(R.id.button_reg)
+        val logLink : TextView = findViewById(R.id.have_acc)
+
+        logLink.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        button.setOnClickListener{
+            val login = userLogin.text.toString().trim()
+            val password = userPassword.text.toString().trim()
+            if(login == "" || password == ""){
+                Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
